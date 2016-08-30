e97ec6c4686edf108846f6577600ca5a4b84cbac
Erica Sadun <erica@ericasadun.com>
2016-08-30 09:59:10 -0600

    updated slightly

--- Sources/SwiftUtility-OptionalComparison.swift
+++ Sources/SwiftUtility-OptionalComparison.swift
@@ -1,9 +1,10 @@
 /// Evaluates the given closure when two `Optional` instances are not `nil`,
-/// passing the unwrapped values as parameters. (Thanks, Mike Ash)
+/// passing the unwrapped values as parameters. 
+/// (Thanks, Mike Ash, Tim Vermeulen)
 fileprivate func _flatMap2<T, U, V>(_ first: T?, _ second: U?, _ transform: (T, U) throws -> V?) rethrows -> V? {
-    return try first.flatMap({ f in
-        try second.flatMap({ s in
-            try transform(f, s) })})
+    return try first.flatMap({ first in
+        try second.flatMap({ second in
+            try transform(first, second) })})
 }
 
 // MARK: Failable Optional Comparisons
@@ -36,6 +37,7 @@ public func>=? <T: Comparable>(lhs: T?, rhs: T?) -> Bool? { return _flatMap2(lhs
 // MARK: Unwrapped Optional Comparisons
 // Following the example of IEEE754 with NAN, any comparison involving
 // .None is false (Thanks, Mike Ash)
+// See also: http://stackoverflow.com/questions/1565164/what-is-the-rationale-for-all-comparisons-returning-false-for-ieee754-nan-values
 
 /// Returns lhs! < rhs!, otherwise false
 public func< <T: Comparable>(lhs: T?, rhs: T?) -> Bool { return lhs <? rhs ?? false }
